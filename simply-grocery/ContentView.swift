6148747359eb7d20c56191f8de7f3e82f8f9e507
Brandon Toy <bgh.toy@gmail.com>
2019-11-30 13:46:50 -0600

    Initial Commit

---
+++ simply-grocery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  simply-grocery
+//
+//  Created by Brandon Toy on 2019-11-30.
+//  Copyright © 2019 Brandon Toy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simply-groceryTests/simply_groceryTests.swift
@@ -0,0 +1,34 @@
+//
+//  simply_groceryTests.swift
+//  simply-groceryTests
+//
+//  Created by Brandon Toy on 2019-11-30.
+//  Copyright © 2019 Brandon Toy. All rights reserved.
+//
+
+import XCTest
+@testable import simply_grocery
+
+class simply_groceryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
